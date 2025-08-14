cb1974b78c597e2f28e55eb6105f9e241ad300c3
khushigarg01 <151988959+khushigarg01@users.noreply.github.com>
2025-08-14 23:58:39 +0530

    Time: 0 ms (100%), Space: 22.3 MB (18.65%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        for (int i = 0; i < asteroids.size(); i++) {
+            if (asteroids[i] > 0) {
+                st.push(asteroids[i]);
+            } else {
+                while (!st.empty() && st.top() > 0 &&
+                       st.top() < abs(asteroids[i])) {
+                    st.pop();
+                }
+                if (!st.empty() && st.top() == abs(asteroids[i])) {
+                    st.pop();
+                } else if (st.empty() || st.top() < 0) {
+                    st.push(asteroids[i]);
+                }
+            }
+        }
+        vector<int> result;
+        while (!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
